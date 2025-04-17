b3a747018938d0410497d6b1ac63baf2d7786318
Renan Rocha <102130210+Renan0Rocha@users.noreply.github.com>
2025-04-17 01:45:56 +0000

    feat: organizaçãos de pastas para utilização de bootstrap

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import path from 'node:path'
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,7 +14,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
     },
   },
 })
